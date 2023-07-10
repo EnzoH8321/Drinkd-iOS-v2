339d5503b16de85672acd170de366108b7ddce62
Enzo Herrera <enzoherrera123@gmail.com>
2023-07-10 13:03:46 -0700

    added card group

---
+++ drinkdV2/Views/BottomTabViews/BottomTabView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomTabView.swift
+//  drinkdV2
+//
+//  Created by Enzo Herrera on 7/10/23.
+//
+
+import SwiftUI
+
+struct BottomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomTabView()
+    }
+}
