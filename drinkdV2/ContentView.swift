87a0ef0fa040ff7484f18ab86056fb136d4bb0d7
Enzo Herrera <enzoherrera123@gmail.com>
2023-07-10 11:32:36 -0700

    Initial Commit

---
+++ drinkdV2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  drinkdV2
+//
+//  Created by Enzo Herrera on 7/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ drinkdV2/drinkdV2App.swift
@@ -0,0 +1,17 @@
+//
+//  drinkdV2App.swift
+//  drinkdV2
+//
+//  Created by Enzo Herrera on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct drinkdV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
